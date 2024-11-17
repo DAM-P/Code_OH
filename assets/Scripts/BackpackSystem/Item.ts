9562ee99e54ced3f88618f437077302da9abff7e
whisper3zzz <3194338947@qq.com>
2024-11-17 19:19:34 +0800

    Item

--- assets/Scripts/BackpackSystem/Item.ts
+++ assets/Scripts/BackpackSystem/Item.ts
@@ -1,29 +1,31 @@
-import { _decorator, Component, Node, SpriteFrame } from 'cc';
+import { _decorator, Component, Node, SpriteFrame, SpriteRenderer } from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('Item')
 export class Item extends Component {
-    @property
+    @property(String)
     itemName: string = '';
 
-    @property
+    @property(Number)
     itemID: number = 0;
 
-    @property(SpriteFrame) 
+    @property(SpriteFrame)
     itemIcon: SpriteFrame = null;// 图标路径
 
-    @property
-    itemCount: number = 1; 
+    @property(Number)
+    itemCount: number = 1;
 
-    @property
+    @property(String)
     itemDescription: string = '';
-    constructor(itemName: string, itemID: number, itemIcon: SpriteFrame, itemCount: number,itemDescription: string) {
+    constructor(itemName: string, itemID: number, itemIcon: SpriteFrame, itemCount: number, itemDescription: string) {
         super();
         this.itemName = itemName;
         this.itemID = itemID;
         this.itemIcon = itemIcon;
         this.itemCount = itemCount;
-        this.itemDescription=itemDescription;
+        this.itemDescription = itemDescription;
+    }
+    protected start(): void {
+        this.node.getComponent(SpriteRenderer).spriteFrame = this.itemIcon;
     }
 }
- 
